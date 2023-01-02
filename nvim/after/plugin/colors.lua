3a5fe032218ed63483f9585f715ff671ce9c29a1
NarayanaRavada <lakshminarayanaravada777@gmail.com>
2023-01-02 14:28:17 +0530

    nvim, tmux files

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,16 @@
+require('rose-pine').setup({
+    disable_background = true,
+    disable_italics = true,
+})
+
+function Colorise(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+vim.g.gruvbox_contrast_dark = 'Hard'
+
+Colorise()
