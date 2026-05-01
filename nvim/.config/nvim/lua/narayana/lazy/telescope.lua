7bd5a843fb407b5dd1eecb3b93444730b5d74001
NarayanaRavada <lakshminarayanaravada777@gmail.com>
2026-05-01 21:17:42 +0530

    fix: to awww, nvim treesitter

--- nvim/.config/nvim/lua/narayana/lazy/telescope.lua
+++ nvim/.config/nvim/lua/narayana/lazy/telescope.lua
@@ -1,32 +1,42 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    version = "*",
+  "nvim-telescope/telescope.nvim",
+  version = "*",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
 
-    config = function()
-        require('telescope').setup({
-            defaults = {
-                winblend = 0,
-            }
-        })
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        winblend = 0,
+      }
+    })
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>fl', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ls', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    local preview_utils = require("telescope.previewers.utils")
+    preview_utils.ts_highlighter = function(bufnr, ft)
+      local lang = vim.treesitter.language.get_lang(ft) or ft
+      if not lang or lang == "" then
+        return false
+      end
+
+      return pcall(vim.treesitter.start, bufnr, lang)
     end
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>fl', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>ls', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+  end
 }
